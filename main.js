a4eeb42f2ef285730c832b37e1928e6cd0c5826e
Maftuna <156097566+maftunarozimatova@users.noreply.github.com>
2024-02-28 18:32:41 +0500

    JS Task

---
+++ main.js
@@ -0,0 +1,30 @@
+// Task 1
+
+let str = "aaa@bbb@ccc"
+let stra = "!"
+
+console.log(
+    str.replace("@", stra).replace("@", stra)
+);
+
+// Task 2
+
+let hello = "HELLOWORLD"
+let world = "hello world"
+console.log(
+    hello = "Hello world" .toLocaleLowerCase()
+);
+
+// Task 3
+
+let js = "Hello, it is HTML"
+console.log(
+    js.replace("HTML", "not JS")
+);
+
+// Task 4
+
+let Alex = "alex"
+console.log(
+    Alex.replace("a", "A")
+);
---
+++ sign.js
@@ -0,0 +1,2 @@
+// Task 1
+let sign = 'aaa@@@bbb@@@ccc';
